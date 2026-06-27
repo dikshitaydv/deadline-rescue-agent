ff0dcf2d4fcb866058d1d3402e744d2b90cd3397
dikshitaydv <dikshitayadav05@gmail.com>
2026-06-27 11:45:47 +0530

    Adde global styles and React entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import Dashboard from './pages/Dashboard'
+
+// Day 2+ will add react-router-dom routes here
+// e.g. <Route path="/rescue/:id" element={<RescuePlan />} />
+
+export default function App() {
+  return <Dashboard />
+}
